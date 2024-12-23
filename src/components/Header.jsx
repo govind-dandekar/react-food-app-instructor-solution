4054129228084b4cba116fdc8ee300363e58afff
Govind Dandekar <govind@Govinds-Mini.lan>
2024-12-22 20:24:28 -0800

    add Meals component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import logoImg from '../assets/logo.jpg'
+
+function Header(){
+	return (
+		<header id="main-header">
+			<div id="title">
+				<img src={logoImg} alt="a restaurant"/>
+				<h1>ReactFood</h1>
+			</div>
+			<nav>
+				<button>
+					Cart (0)
+				</button>
+			</nav>
+		</header>
+	)
+}
+
+export default Header;
